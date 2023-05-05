033aa1ab06917fa21c77cca035801eb1a0edb36f
Ducanger <thanhdatcyo@gmail.com>
2023-05-05 15:37:28 +0700

    scheduled backup database

---
+++ restore_mysql.bat
@@ -0,0 +1,5 @@
+set mysql_user=root
+set mysql_password=dtd2002
+set mysql_port=3308
+
+mysql --user=%mysql_user% --password=%mysql_password% --port=%mysql_port% w22g7_geek < D:\Project\book\2023-05-05_10-47_database.sql
